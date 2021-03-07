6e18496f2dbc951b8425bea100125d73b9933d22
Andy Park <apark1214@gmail.com>
2021-03-07 11:48:27 -0800

    basic set up complete

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Home from './pages/home';
+import Testing from './component/testing';
 
 export default class App extends React.Component {
   render() {
-    return <Home />;
+    return <Testing />;
   }
 }
