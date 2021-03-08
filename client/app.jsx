7f6a97835d7efb3f3bff9e5097aa93efa21f0a69
Andy Park <apark1214@gmail.com>
2021-03-07 20:55:39 -0800

    adding dnd function

--- client/app.jsx
+++ client/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 const characters =
   [
     {
@@ -11,20 +11,22 @@ const characters =
       id: 'Golden-Retriever',
       name: 'Lemon',
       pic: '/images/golden.jpg'
+    },
+    {
+      id: 'Austrailian',
+      name: 'Peach',
+      pic: '/images/austrailian.jpg'
     }
   ];
 
-const renderIt = () => {
+const renderIt = provided => {
   const animals = characters.map((value, index) => {
     return (
-      <div key={index} className="col-12 d-flex">
-        <div className="picture">
-          <img src={value.pic} alt={value.name} />
-        </div>
-        <div className="information">
-          <h3>{value.name}</h3>
-          <h3>{value.id}</h3>
-        </div>
+      <div key={index} className="col-12 d-flex  align-items-center w-25 border border-secondary mt-3"{...provided.droppableProps} ref={provided.innerRef}>
+          <img className="picture" src={value.pic} alt={value.name} />
+        <ul className="information ">
+          <li>{value.id}</li>
+        </ul>
       </div>
     );
   });
@@ -33,9 +35,16 @@ const renderIt = () => {
 
 const App = () => {
   return (
-    <div className="container">
-      <div className="row">
-        {renderIt()}
+    <div className="container d-flex justify-content-center">
+      <div className="row d-flex justify-content-center w-50">
+        <h1>Dogs That I want</h1>
+        <DragDropContext>
+          <Droppable droppableId="characters">
+            {provided => {
+              renderIt(provided);
+            }}
+          </Droppable>
+        </DragDropContext>
       </div>
     </div>
   );
