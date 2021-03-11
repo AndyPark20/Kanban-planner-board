10b9ad9a1c443bce492213c5618ec0af106076de
Andy Park <apark1214@gmail.com>
2021-03-10 20:15:38 -0800

    combining column and app components together

--- client/app.jsx
+++ client/app.jsx
@@ -2,60 +2,6 @@ import React, { useState, useEffect } from 'react';
 import Column from './component/column';
 
 const App = () => {
-  const characters = {
-    todo: {
-      id: 'todo',
-      list: [{ name: 'gogo', img: 'images/pek1.jpg' }, { name: 'Lemon', img: 'images/austrailian.jpg' }, { name: 'Penny', img: 'images/golden.jpg' }]
-    },
-    doing: {
-      id: 'doing',
-      list: []
-    },
-    done: {
-      id: 'done',
-      list: []
-    }
-  };
-
-  const [character, updateCharacters] = useState(characters);
-
-  useEffect(() => {
-    // console.log('Overall object', character);
-  }, [character]);
-
-  const allowDrop = e => {
-    e.preventDefault();
-  };
-
-  const dropIt = (e, info, position) => {
-    e.preventDefault();
-    const id = e.dataTransfer.getData('id');
-    const name = e.dataTransfer.getData('name');
-    const img = e.dataTransfer.getData('img');
-    if (character[info.id].id !== id) {
-      if (e.target.nodeName !== 'IMG') {
-        character[info.id].list.push({ name: name, img: img });
-        const returnedObject = Object.assign({}, character);
-        character[id].list.forEach((values, index) => {
-          if (values.name === name) {
-            character[id].list.splice(index, 1);
-          }
-        });
-        updateCharacters(returnedObject);
-      }
-    }
-  };
-
-  const renderIt = () => {
-    const loop = Object.values(character).map((info, index) => {
-      return (
-        <div key={index} onDragOver={e => allowDrop(e)} onDrop={e => dropIt(e, info, index)}>
-          <Column values={info} key={index} column={index} />
-        </div>
-      );
-    });
-    return loop;
-  };
 
   return (
     <div className="columnCustom">
--- client/component/column.jsx
+++ client/component/column.jsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Item from './item';
 
-const Column = ({ values: { list, id, column } }) => {
-
-  const [ordered, setOrdered] = useState(list);
-
+const Column = () => {
   const characters = {
     todo: {
       id: 'todo',
@@ -20,49 +17,47 @@ const Column = ({ values: { list, id, column } }) => {
     }
   };
 
-  const [character, updateCharacters] = useState(characters);
+  // const [ordered, setOrdered] = useState(list);
+  // const [character, updateCharacters] = useState(characters);
 
-  useEffect(() => {
-    // console.log('Overall object', character);
-  }, [character]);
 
-  const allowDrop = e => {
-    e.preventDefault();
-  };
+  // const allowDrop = e => {
+  //   e.preventDefault();
+  // };
 
-  const dropIt = (e, info, position) => {
-    e.preventDefault();
-    const id = e.dataTransfer.getData('id');
-    const name = e.dataTransfer.getData('name');
-    const img = e.dataTransfer.getData('img');
-    if (character[info.id].id !== id) {
-      if (e.target.nodeName !== 'IMG') {
-        character[info.id].list.push({ name: name, img: img });
-        const returnedObject = Object.assign({}, character);
-        character[id].list.forEach((values, index) => {
-          if (values.name === name) {
-            character[id].list.splice(index, 1);
-          }
-        });
-        updateCharacters(returnedObject);
-      }
-    }
-  };
+  // const dropIt = (e, info, position) => {
+  //   e.preventDefault();
+  //   const id = e.dataTransfer.getData('id');
+  //   const name = e.dataTransfer.getData('name');
+  //   const img = e.dataTransfer.getData('img');
+  //   if (character[info.id].id !== id) {
+  //     if (e.target.nodeName !== 'IMG') {
+  //       character[info.id].list.push({ name: name, img: img });
+  //       const returnedObject = Object.assign({}, character);
+  //       character[id].list.forEach((values, index) => {
+  //         if (values.name === name) {
+  //           character[id].list.splice(index, 1);
+  //         }
+  //       });
+  //       updateCharacters(returnedObject);
+  //     }
+  //   }
+  // };
 
-  const renderIt = () => {
-    const loop = Object.values(character).map((info, index) => {
-      return (
-        <div key={index} onDragOver={e => allowDrop(e)} onDrop={e => dropIt(e, info, index)}>
-          <Column values={info} key={index} column={index} />
-        </div>
-      );
-    });
-    return loop;
-  };
+  // const listing = () => {
+  //   const renderIt = ordered.map((values, index) => {
+  //     return (
+  //       <div key={index} draggable onDragStart={e => controlDragStart(e, values, index)} onDrop={e => lastIndex(e, index)} >
+  //         <Item value={values.name} img={values.img} />
+  //       </div>
+  //     );
+  //   });
+  //   return renderIt;
+  // };
+  // onDragOver = { e => allowDrop(e)} onDrop = { e => dropIt(e, info, index) }
 
-  /** **the column part */
+/** **the column part */
   const lastIndex = (e, index) => {
-    console.log(column);
     const startIndex = e.dataTransfer.getData('startIndex');
     const finishedIndex = index;
     const [reordered] = list.splice(startIndex, 1);
@@ -72,35 +67,46 @@ const Column = ({ values: { list, id, column } }) => {
 
   };
 
-  const controlDragStart = (e, values, index) => {
-    e.dataTransfer.setData('name', values.name);
-    e.dataTransfer.setData('img', values.img);
-    e.dataTransfer.setData('id', id);
-    e.dataTransfer.setData('startIndex', index);
-  };
+  // const controlDragStart = (e, values, index) => {
+  //   e.dataTransfer.setData('name', values.name);
+  //   e.dataTransfer.setData('img', values.img);
+  //   e.dataTransfer.setData('id', id);
+  //   e.dataTransfer.setData('startIndex', index);
+  // };
 
-  const listing = () => {
-    const renderIt = ordered.map((values, index) => {
+
+  const renderIt = () => {
+    const loop = Object.values(characters).map((info, index) => {
       return (
-          <div key={index} draggable onDragStart={e => controlDragStart(e, values, index)} onDrop={e => lastIndex(e, index)} >
-            <Item value={values.name} img={values.img} />
+        <div key={index}  className="col-4 d-flex text-center align-items-center flex-column justify-content-around">
+          <h2>{info.id}</h2>
+          <div className="border border-danger w-100 columnCustom  d-flex flex-column">
+            {info.list.map((values, index) => {
+              return(
+                <div key={index} draggable onDragStart={e=>lastIndex(e,index)}>
+                <img className="pictureCustom" src={values.img} alt="dogs" />
+                </div>
+              )
+            })}
           </div>
+        </div>
       );
     });
-    return renderIt;
+    return loop;
   };
 
+
   return (
-   <div className="container">
-      <div className="row d-flex flex-column align-items-center">
-       <div className="col-4">
-         <h2>{id}</h2>
-       </div>
+    <div className="container w-100">
+      <div className="row  w-100">
+        {renderIt()}
+      </div>
+      {/*
         <div className="border border-danger w-50 custom">
         {listing()}
       </div>
-     </div>
-   </div>
+     </div> */}
+    </div>
 
   );
 
