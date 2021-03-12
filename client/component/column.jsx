1ef93419b3724465f292dd42b5ab458ee9301681
Andy Park <apark1214@gmail.com>
2021-03-12 07:54:27 -0800

    inn prgoress to make columns switchable

--- client/component/column.jsx
+++ client/component/column.jsx
@@ -19,15 +19,15 @@ const Column = () => {
 
   const [character, updateCharacters] = useState(characters);
   const [columnMover, updateColumnMover] = useState(false);
+  const [packageColumn, updatePackageColumn] = useState(false);
 
   const dropIt = (e, info, position) => {
     // the column index number
     e.preventDefault();
-
     const identity = e.dataTransfer.getData('name');
     const imgs = e.dataTransfer.getData('img');
     const originId = e.dataTransfer.getData('originId');
-    if (character[info.id].id !== originId) {
+    if (character[info.id].id !== originId && columnMover && !packageColumn) {
       if (e.target.nodeName !== 'IMG') {
         character[info.id].list.push({ img: imgs, name: identity });
         const returnedObjects = Object.assign({}, character);
@@ -42,6 +42,7 @@ const Column = () => {
         });
       }
     }
+
   };
 
   /** **the column part */
@@ -60,6 +61,9 @@ const Column = () => {
   };
 
   const controlDragStart = (e, values, info, index) => {
+    console.log('item');
+    updatePackageColumn(false);
+    updateColumnMover(true);
     e.dataTransfer.setData('originId', info.id);
     e.dataTransfer.setData('name', values.name);
     e.dataTransfer.setData('img', values.img);
@@ -75,23 +79,21 @@ const Column = () => {
 
   // functions to move columns around
 
-  const changeColumn = (e, index) => {
-    const startIndex = e.dataTransfer.getData('columnStartIndex');
-  };
-
   const moveColumn = (e, index) => {
+    // updateColumnMover(false);
+    // updatePackageColumn(true);
     e.dataTransfer.setData('columnStartIndex', index);
   };
 
   const renderIt = () => {
     const loop = Object.values(character).map((info, index) => {
       return (
-        <div key={index} className="col-4 d-flex text-center flex-column justify-content-around w-100 border select" draggable onDragStart={e => moveColumn(e, index)} onDrag={e => allowDrop(e)} onDrop={e => console.log('hello')}>
+        <div key={index} className="col-4 d-flex text-center flex-column justify-content-around w-100 border select" draggable onDragStart={e => moveColumn(e, index)} onDrag={e => allowDrop(e)} onDrop={e => dropIt(e, info, index)}>
           <div className="d-flex align-items-end justify-content-around w-100">
             <h2 className="fontColor">{info.id}</h2>
             <h6 className="point fontColor" onClick={e => makeNewItem(e, info)}>add</h6>
           </div>
-          <div className=" columnBackground w-100 columnCustom d-flex flex-column" onDragOver={e => allowDrop(e)} onDrop={e => dropIt(e, info, index)} >
+          <div className=" columnBackground w-100 columnCustom d-flex flex-column" onDragOver={e => allowDrop(e)} >
             {info.list.map((values, index) => {
               return (
                 <div key={index} draggable onDragStart={e => controlDragStart(e, values, info, index)} onDrag={e => allowDrop(e)} onDrop={e => lastIndex(e, info, index)} >
