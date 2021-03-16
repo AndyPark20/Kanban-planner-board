d09aa654e294a5064116369e51652ebc4ae4eb3c
Andy Park <apark1214@gmail.com>
2021-03-15 18:00:43 -0700

    re-add the modified column-mover code

--- client/component/column.jsx
+++ client/component/column.jsx
@@ -2,20 +2,21 @@ import React, { useState, useEffect } from 'react';
 import Item from './item';
 
 const Column = () => {
-  const characters = {
-    todo: {
-      id: 'todo',
+  const characters = [
+    {
+      id: 'Todo',
+      list: []
+
+    },
+    {
+      id: 'Doing',
       list: []
     },
-    doing: {
-      id: 'doing',
-      list: []
-    },
-    done: {
-      id: 'done',
+    {
+      id: 'Done',
       list: []
     }
-  };
+  ];
 
   const [character, updateCharacters] = useState(characters);
   const [columnMover, updateColumnMover] = useState(false);
@@ -25,27 +26,28 @@ const Column = () => {
     e.preventDefault();
     const identity = e.dataTransfer.getData('name');
     const imgs = e.dataTransfer.getData('img');
-    const originId = e.dataTransfer.getData('originId');
-    if (character[info.id].id !== originId && !columnMover) {
+    const originId = e.dataTransfer.getData('startIndex');
+    const columnStartIndex = e.dataTransfer.getData('columnStartIndex');
+    if (character.id !== originId && !columnMover) {
       if (e.target.nodeName !== 'IMG') {
-        character[info.id].list.push({ img: imgs, name: identity });
-        const returnedObjects = Object.assign({}, character);
+        character[position].list.push({ img: imgs, name: identity });
+        const returnedObjects = character.concat();
         updateCharacters(returnedObjects);
-        character[originId].list.forEach((values, index) => {
+        character[columnStartIndex].list.forEach((values, index) => {
           if (values.name === identity) {
-            character[originId].list.splice(index, 1);
+            character[columnStartIndex].list.splice(index, 1);
           }
         });
       }
     } else {
-      const originCol = e.dataTransfer.getData('originColumn');
-      console.log(character);
-      console.log(position);
-      console.log(character[originCol].id);
-      const movedColumnObject = Object.values(character).map((description, value) => {
+      const originCol = e.dataTransfer.getData('columnStartIndex');
+      character.forEach((description, value) => {
         if (description.id === character[originCol].id) {
-          console.log(description);
-          console.log(value);
+          const swap = character[position];
+          character[position] = character[value];
+          character[value] = swap;
+          const swappedResult = character.concat();
+          updateCharacters(swappedResult);
         }
       });
     }
@@ -56,8 +58,9 @@ const Column = () => {
     e.preventDefault();
     const startIndex = e.dataTransfer.getData('startIndex');
     const finishedIndex = index;
-    const [reordered] = character[info.id].list.splice(startIndex, 1);
-    character[info.id].list.splice(finishedIndex, 0, reordered);
+
+    const [reordered] = character.list.splice(startIndex, 1);
+    character.list.splice(finishedIndex, 0, reordered);
     const returnedObject = Object.assign({}, character);
     updateCharacters(returnedObject);
   };
@@ -67,17 +70,15 @@ const Column = () => {
   };
 
   const controlDragStart = (e, values, info, index) => {
-    updatePackageColumn(false);
-    updateColumnMover(true);
     e.dataTransfer.setData('originId', info.id);
     e.dataTransfer.setData('name', values.name);
     e.dataTransfer.setData('img', values.img);
     e.dataTransfer.setData('startIndex', index);
   };
 
-  const makeNewItem = (e, info) => {
-    character[info.id].list.push({ name: 'gogo', img: 'images/pek1.jpg' });
-    const addedCardObject = Object.assign({}, character);
+  const makeNewItem = (e, info, index) => {
+    character[index].list.push({ name: 'gogo', img: 'images/pek1.jpg' });
+    const addedCardObject = character.concat();
     updateCharacters(addedCardObject);
   };
 
@@ -88,18 +89,18 @@ const Column = () => {
       e.dataTransfer.setData('columnStartIndex', value);
     } else {
       updateColumnMover(true);
-      e.dataTransfer.setData('originColumn', info.id);
+      e.dataTransfer.setData('columnStartIndex', value);
     }
 
   };
 
   const renderIt = () => {
-    const loop = Object.values(character).map((info, index) => {
+    const loop = character.map((info, index) => {
       return (
         <div key={index} className="col-4 d-flex text-center flex-column justify-content-around w-100 border select" draggable onDragStart={e => moveColumn(e, info, index)} onDrag={e => allowDrop(e)} onDrop={e => dropIt(e, info, index)}>
           <div className="d-flex align-items-end justify-content-around w-100">
             <h2 className="fontColor">{info.id}</h2>
-            <h6 className="point fontColor" onClick={e => makeNewItem(e, info)}>add</h6>
+            <h6 className="point fontColor" onClick={e => makeNewItem(e, info, index)}>add</h6>
           </div>
           <div className=" columnBackground w-100 columnCustom d-flex flex-column" onDragOver={e => allowDrop(e)} >
             {info.list.map((values, index) => {
