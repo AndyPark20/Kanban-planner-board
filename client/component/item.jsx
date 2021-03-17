f56ea5b0a8e660e739cc2da2b9fb7b472753003c
Andy Park <apark1214@gmail.com>
2021-03-16 20:12:49 -0700

    fixed alignment on the cards

--- client/component/item.jsx
+++ client/component/item.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 const Item = ({ values }) => {
 
   return (
-    // <div draggable>
-    //   <img className="pictureCustom" src={values.img} alt="dogs" />
-    // </div>
+
     <div className="card" draggable>
       <img className="card-img-top" src={values.img} alt="Card image cap" />
       <div className="card-body">
@@ -18,12 +16,3 @@ const Item = ({ values }) => {
 };
 
 export default Item;
-
-{ /* <div className="card" style="width: 18rem;">
-  <img className="card-img-top" src="..." alt="Card image cap" />
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div> */ }
