794802a5fc65529134947ecb6b95923ede4bf3d1
Andy Park <apark1214@gmail.com>
2021-03-18 14:15:45 -0700

    working on backend api for wallpapers

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(staticMiddleware);
 
+app.get('api/picture/:query/:orientation/:size', (req, res, next) => {
+  console.log(req.params);
+});
+
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`express server listening on port ${process.env.PORT}`);
