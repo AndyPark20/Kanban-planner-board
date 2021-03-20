aad4883685fb5b13fbdcf720048f03a2108ed47e
Andy Park <apark1214@gmail.com>
2021-03-19 19:51:36 -0700

    adding side naviation bar

---
+++ client/options.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+export default class options extends React.Component{
+  constructor(props){
+    super(props);
+    this.state=({property:0});
+  }
+
+  return{
+
+  }
+}
