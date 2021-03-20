5e4bd443892c2f8a8f42df60dc87eb66117fc951
Andy Park <apark1214@gmail.com>
2021-03-19 19:33:46 -0700

    added styles background propeerty on div

--- client/app.jsx
+++ client/app.jsx
@@ -5,7 +5,10 @@ const App = () => {
 
   return (
     <div style={{
-      backgroundImage: 'url(\'https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1440&w=2500\')'
+      backgroundImage: 'url(\'https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1440&w=2500\')',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'auto',
+      height: '100vh'
     }}>
       <div className="columnCustom">
         <Column />
