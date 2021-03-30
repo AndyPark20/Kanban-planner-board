4c210bd8498f41892ba8a5cb2cdd8fd455da620c
Andy Park <apark1214@gmail.com>
2021-03-30 15:46:26 -0700

    added input bar for title

--- client/component/item.jsx
+++ client/component/item.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 
 const Item = () => {
 
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  const enterTitle = e => {
+    if (e.key === 'Enter') {
+
+    }
+  };
+
   return (
     <div className="card spacing cardStyle" draggable>
       {/* <img className="card-img-top" src={values.img} alt="Card image cap" /> */}
-      <div className="card-body">
-      <input type="text" placeholder="Enter a title for this card" className="titleEnter"></input>
-      </div>
+      <h5 className="card-title">Hello</h5>
+      <form onSubmit={e => handleSubmit(e)}>
+          <input type="text" placeholder="Enter a title for this card" className="titleEnter" onKeyUp={e => enterTitle(e)}></input>
+      </form>
     </div>
   );
 };
