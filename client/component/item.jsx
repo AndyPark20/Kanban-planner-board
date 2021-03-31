d5bbe7864fa8e86e3f018476b773af0ccee89208
Andy Park <apark1214@gmail.com>
2021-03-31 15:20:54 -0700

    added pencil icon for update on cards

--- client/component/item.jsx
+++ client/component/item.jsx
@@ -23,12 +23,15 @@ const Item = ({ cardName, userCardTitle, cardSequence, columnNumber, masterChara
 
   return (
     <div className="card spacing" draggable>
-      <div className="card-body">
-      <h5 className="card-title">{values.name}</h5>
-      <form onSubmit={e => handleSubmit(e)}>
+      <div className="card-body border border-primary">
+        <div className="text-right">
+          <i className="fas fa-pencil-alt"></i>
+        </div>
+        <h5 className="card-title">{values.name}</h5>
+        <form onSubmit={e => handleSubmit(e)}>
           <input type="text" placeholder="Enter a title for this card" className={hideTitleEdit()} onKeyUp={e => enterTitle(e)} required></input>
-      </form>
-    </div>
+        </form>
+      </div>
     </div>
   );
 };
