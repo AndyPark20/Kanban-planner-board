4e764a9bbd2272773de208b9b6fb605ceb683ca8
Andy Park <apark1214@gmail.com>
2021-03-31 22:14:30 -0700

    in progress to make udpated titles show on header of the modal

--- client/component/modalList.jsx
+++ client/component/modalList.jsx
@@ -4,12 +4,20 @@ import React from 'react';
 export default class modal extends React.Component {
   constructor(props) {
     super(props);
-    this.state = ({ value: 'hello', modalStatus: false });
+    this.state = ({ value: '', modalStatus: false });
     this.modalEffect = this.modalEffect.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.switchModal = this.switchModal.bind(this);
     this.switchCardTitle = this.switchCardTitle.bind(this);
     this.updateCardTitle = this.updateCardTitle.bind(this);
+  }
+
+  componentDidUpdate(prev) {
+    const column = this.props.columnNumber;
+    const card = this.props.cardNumber;
+    if (this.state.value !== this.props.masterCharacter && prev.masterCharacter.length !== 0) {
+      this.setState({ value: prev.masterCharacter[column].list[card].name });
+    }
   }
 
   modalEffect() {
@@ -63,7 +71,7 @@ export default class modal extends React.Component {
           <div className="col pt-2">
             <div className="d-flex align-items-center pl-2">
               <i className="fas fa-tasks logoSize"></i>
-              <h3 className={this.switchCardTitle()}>{this.selectedListInfo()}</h3>
+              <h3 className={this.switchCardTitle()}>{this.state.value}</h3>
               <p className={this.switchCardTitle()} onClick={() => this.setState({ value: character[column].list[card].name, modalStatus: true })}>edit</p>
               <input text="type" className={this.switchModal()} value={this.state.value} onChange={this.handleSubmit} onKeyUp={e => this.updateCardTitle(e)}></input>
             </div>
