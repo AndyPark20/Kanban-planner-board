42e5d27fff89d0a3a2254ac7899c8b4f2d3307a3
Andy Park <apark1214@gmail.com>
2021-04-10 17:40:28 -0700

    adding userLog Feature

--- client/component/activity.jsx
+++ client/component/activity.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Activity = () => {
+
+  const userAtivity = e => {
+    e.preventDefault();
+    console.log(e.target.value);
+  };
+
   return (
     <div>
       <div className="d-flex align-items-center pl-2">
         <i className="fas fa-chart-line"></i>
         <h3 className="pl-2">Activity</h3>
       </div>
-      <form>
+      <form onChange={e => userAtivity(e)}>
         <textarea className="form-control w-75" rows="1"></textarea>
+        <button type="submit" className="btn btn-success mt-2">Save</button>
       </form>
     </div>
   );
