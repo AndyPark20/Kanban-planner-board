417843c38b99c0e1b4b98ec58a6738cc75639afc
Andy Park <apark1214@gmail.com>
2021-04-12 13:01:09 -0700

    adding edit feature on activity log

--- client/component/activity.jsx
+++ client/component/activity.jsx
@@ -1,37 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import Moment  from 'react-moment';
+import Moment from 'react-moment';
 
 const Activity = () => {
 
   const [userLog, updateUserLog] = useState('');
-  const [userLogSubmit, updateUserLogSubmit]= useState([])
+  const [userLogSubmit, updateUserLogSubmit] = useState([]);
 
   const userActivity = e => {
     e.preventDefault();
-    updateUserLog({info:e.target.value, time: Date.now()})
+    updateUserLog({ info: e.target.value, time: Date.now() });
   };
 
-  useEffect(()=>{
-    console.log(userLogSubmit)
-  },[userLogSubmit])
+  useEffect(() => {
+    console.log(userLogSubmit);
+  }, [userLogSubmit]);
 
   const renderLog = () => {
     const data = userLogSubmit.map((values, index) => {
       return (
         <div key={index} className="d-flex align-items-center">
         <i className="far fa-comment-dots"></i>
-      <h6 className="pl-2">{values.info}</h6>
+      <h5 className="pl-2">{values.info}</h5>
           <Moment className="timeFontSize pl-2" format='YYYY/MM/DD hh:mm:ss'>{values.time}</Moment>
+          <h6 className="pl-2 editActivity">Edit</h6>
         </div>
       );
     });
     return data;
   };
 
-  const userSave =(e)=>{
-    e.preventDefault()
-    updateUserLogSubmit(userLogSubmit.concat(userLog))
-  }
+  const userSave = e => {
+    e.preventDefault();
+    updateUserLogSubmit(userLogSubmit.concat(userLog));
+  };
 
   return (
     <div>
@@ -44,7 +45,7 @@ const Activity = () => {
       </div>
       <form onChange={e => userActivity(e)}>
         <textarea className="form-control w-75" rows="1"></textarea>
-        <button type="submit" className="btn btn-success mt-2" onClick={(e)=>userSave(e)}>Save</button>
+        <button type="submit" className="btn btn-success mt-2" onClick={e => userSave(e)}>Save</button>
       </form>
     </div>
   );
