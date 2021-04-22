c39b042b88319f11463c9372f2de3d897f7c57bd
Andy Park <apark1214@gmail.com>
2021-04-21 18:59:12 -0700

    in converting

--- client/component/modalListRevised.jsx
+++ client/component/modalListRevised.jsx
@@ -17,13 +17,37 @@ const Modal = () => {
     return 'form-control w-75';
   }
 
-  function modalEffect(){
+  function modalEffect() {
     if (this.state.modalClose) {
       return 'container centerModal';
     }
     if (!this.state.modalClose) {
       return 'container centerModal hidden';
     }
+  }
+
+  function descriptionInfo(e) {
+    e.preventDefault();
+    if (e.key === 'Enter' || e.target.className === 'btn btn-success mt-2') {
+      this.setState({ finalDescription: this.state.initialDescription, descriptionStatus: true });
+    }
+    if (e.target.value) {
+      this.setState({ button: true });
+    } else {
+      this.setState({ button: false });
+    }
+  }
+
+  function saveBtn() {
+    this.setState({ textValue: true });
+  }
+
+  function switchCardTitle() {
+    if (this.state.modalStatus) {
+      return 'hidden';
+    }
+    return 'pl-2';
+  }
 
 };
 
