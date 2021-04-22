a1c6849bf207e6ae4d748b9362736500421f3c23
Andy Park <apark1214@gmail.com>
2021-04-21 19:04:33 -0700

    adding useState

--- client/component/modalListRevised.jsx
+++ client/component/modalListRevised.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Activity from './activity';
 
 const Modal = () => {
-
+  const [values, updateValue] = useState('');
   function infoDescription() {
     if (this.state.descriptionStatus) {
       return 'pl-4';
