46bf23580ce12f9c679225d8b12ee876d5d84b38
Andy Park <apark1214@gmail.com>
2021-04-24 14:30:48 -0700

    locating title name from the column component (parent) of item component

--- client/component/item.jsx
+++ client/component/item.jsx
@@ -12,6 +12,7 @@ const Item = ({ cardName, userCardTitle, cardSequence, columnNumber, masterChara
   };
 
   const enterTitle = e => {
+
     if (e.key === 'Enter' && e.target.value !== '' && columnNumber !== undefined) {
       masterCharacter[columnNumber].list[cardSequence] = { name: e.target.value };
       update(masterCharacter);
@@ -22,8 +23,7 @@ const Item = ({ cardName, userCardTitle, cardSequence, columnNumber, masterChara
   };
 
   const openModalComponent = () => {
-    const cardTitleName = masterCharacter[columnNumber].list[cardSequence].name;
-    if (cardTitleName) {
+    if (openModal) {
       updateModal(true);
     }
   };
