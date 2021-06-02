c794b84a2eca8b84246778f9b751e3866a3cd267
AndyPark20 <apark1214@gmail.com>
2021-06-02 04:34:38 +0000

    working on re-routing app.jsx

--- client/app.jsx
+++ client/app.jsx
@@ -1,14 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import Home from './home';
 import ParseRoute from '../client/component/library/parse-route'
+import { hash } from 'argon2';
+import TestIt from './testIt'
 
-const [currentUrl, updateCurrentUrl] = useState('')
 
-function renderPage(){
+const  App = ()=> {
+  const [currentUrl, updateCurrentUrl] = useState('');
 
+  useEffect(() => {
+    window.addEventListener('hashchange', () => {
+      const changedHash = window.location.hash;
+      const parsed = ParseRoute(changedHash);
+      updateCurrenUrl(parsed);
+    })
+  })
+
+  const renderPage =()=>{
+    if(currentUrl.path ==='Home'){
+      <Home />
+    }
+    if(currentUrl.path ==='Login'){
+      <TestIt />
+    }
+  }
+
+  return (
+    <div>
+
+    </div>
+  )
 }
-
-
 
 
 export default App;
