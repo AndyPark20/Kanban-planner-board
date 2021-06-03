69abff413939196fe8f6c3ef7fa733483d79ca6b
Andy Park <apark1214@gmail.com>
2021-06-02 22:03:09 -0700

    created user and password

--- client/login.jsx
+++ client/login.jsx
@@ -7,13 +7,13 @@ const HomeEntry = () => {
 
   return (
     <div className="container">
-      <div className="row d-flex flex-column justify-content-center text-center">
+      <div className="row d-flex flex-column justify-content-center">
         <div className="type-column d-flex flex-column">
-          <label>UserName:</label>
+          <label className="title-margin">UserName:</label>
             <input name="username" className="input-width" type="text"></input>
         </div>
-        <div className="type-column d-flex flex-column">
-          <label>Password:</label>
+        <div className="type-column d-flex flex-column mt-5">
+          <label className="title-margin">Password:</label>
           <input name="password" className="input-width" type="password"></input>
         </div>
       </div>
