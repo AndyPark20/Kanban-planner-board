20eba5c6801232fcefde01889dc25c88bd57a072
Andy Park <apark1214@gmail.com>
2021-06-07 20:46:19 -0700

    working on setting values to the input

--- client/login.jsx
+++ client/login.jsx
@@ -5,16 +5,25 @@ const HomeEntry = () => {
 
   const [userName, updateUserName] = useState('');
 
+  useEffect(() => {
+    console.log('username', userName);
+  });
+
+  const handleSubmit = e => {
+    updateUserName(e.target.value);
+    e.preventDefault();
+  };
+
   return (
     <div className="container">
       <div className="row d-flex flex-column justify-content-center">
         <div className="type-column d-flex flex-column ">
           <form>
             <label className="title-margin">UserName:</label>
-            <input name="username" className="input-width" type="text"></input>
+            <input name="username" className="input-width" type="text" ></input>
             <div className="pt-2">
               <label className="title-margin">Password:</label>
-              <input name="password" className="input-width" type="password"></input>
+              <input name="password" className="input-width" type="password" onChange={e => handleSubmit(e)}></input>
             </div>
             <div className="text-right mt-5">
               <button type="submit" className="btn btn-primary margin">Submit</button>
