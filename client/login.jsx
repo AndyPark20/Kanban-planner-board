045165872d1c29a07f05e8240ef6310072d005f5
Andy Park <apark1214@gmail.com>
2021-06-07 20:33:50 -0700

    making submit button

--- client/login.jsx
+++ client/login.jsx
@@ -9,12 +9,12 @@ const HomeEntry = () => {
     <div className="container">
       <div className="row d-flex flex-column justify-content-center">
         <div className="type-column d-flex flex-column">
-          <label className="title-margin">UserName:</label>
+          <form>
+            <label className="title-margin">UserName:</label>
             <input name="username" className="input-width" type="text"></input>
-        </div>
-        <div className="type-column d-flex flex-column mt-5">
-          <label className="title-margin">Password:</label>
-          <input name="password" className="input-width" type="password"></input>
+            <label className="title-margin">Password:</label>
+            <input name="password" className="input-width" type="password"></input>
+          </form>
         </div>
       </div>
     </div>
