2d2ac07467a9f0ef7b96f5070167cb7911333e41
Andy Park <apark1214@gmail.com>
2021-06-07 20:42:53 -0700

    aligned button

--- client/login.jsx
+++ client/login.jsx
@@ -8,7 +8,7 @@ const HomeEntry = () => {
   return (
     <div className="container">
       <div className="row d-flex flex-column justify-content-center">
-        <div className="type-column d-flex flex-column border border-danger">
+        <div className="type-column d-flex flex-column ">
           <form>
             <label className="title-margin">UserName:</label>
             <input name="username" className="input-width" type="text"></input>
@@ -16,8 +16,8 @@ const HomeEntry = () => {
               <label className="title-margin">Password:</label>
               <input name="password" className="input-width" type="password"></input>
             </div>
-            <div className="text-right">
-              <button type="submit">Submit</button>
+            <div className="text-right mt-5">
+              <button type="submit" className="btn btn-primary margin">Submit</button>
             </div>
           </form>
         </div>
