a75ddde9b29a33de6bd7dd7c16422c324bfe2a19
Andy Park <apark1214@gmail.com>
2021-06-10 21:59:46 -0700

    in progress

--- server/index.js
+++ server/index.js
@@ -10,13 +10,12 @@ app.use(cors());
 app.use(staticMiddleware);
 
 app.get('/api/picture/:query/:orientation/:size', (req, res, next) => {
-  console.log('picture time');
-  // fetch(`https://api.pexels.com/v1/search?query=${req.params.query}&orientation=${req.params.orientation}&size=${req.params.size}`, {
-  //   method: 'GET',
-  //   headers: {
-  //     'Content-type': 'application/json',
-  //     Authorization: '563492ad6f917000010000010af25f7c94cc48d29741c25d8bf6aa0f'
-  //   }
+  fetch(`https://api.pexels.com/v1/search?query=${req.params.query}&orientation=${req.params.orientation}&size=${req.params.size}`, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json',
+      Authorization: '563492ad6f917000010000010af25f7c94cc48d29741c25d8bf6aa0f'
+    }
   })
 
     .then(res => {
@@ -32,8 +31,8 @@ app.get('/api/picture/:query/:orientation/:size', (req, res, next) => {
 // });
 
 // TESTING LOG IN
-app.get('/logIn', (req, res, next) => {
-  console.log('hello');
+app.post('/submit', (req, res, next) => {
+  console.log('working');
 
 });
 
