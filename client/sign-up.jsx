d0c18435d2417c2c5dc0ef65b227a05aa2a731a9
Andy Park <apark1214@gmail.com>
2021-06-12 13:25:14 -0700

    completed sign-up forms

--- client/sign-up.jsx
+++ client/sign-up.jsx
@@ -18,11 +18,22 @@ const signUp = () => {
       <div className="row d-flex flex-column justify-content-center">
         <div className="type-column d-flex flex-column align-items-center">
           <form onSubmit={handleSubmit} className="d-flex flex-column w-50">
+            {/* Firstname */}
             <label htmlFor="firstname">Firstname:</label>
             <input type="text" name="firstname" value={firstName} onChange={handleChange}></input>
-              <label className="mt-2">Lastname:</label>
-              <input htmlFor="lastname" type="text" name="lastname" value={lastName} onChange={handleChange}></input>
-            <div className="mt-2 d-flex justify-content-end custom-margin">
+            {/* lastname */}
+            <label htmlFor="lastname" className="mt-2">Lastname:</label>
+            <input name="lastname" type="text" value={lastName} onChange={handleChange}></input>
+            {/* Username */}
+            <label htmlFor="userName" className="mt-2">Username:</label>
+            <input name="userName" type="text"></input>
+            {/* Password */}
+            <label htmlFor="password" className="mt-2">Password:</label>
+            <input name="password" type="password"></input>
+            {/* re-enter password */}
+            <label htmlFor="password" className="mt-2">Confirm Password:</label>
+            <input name="password" type="password"></input>
+            <div name="userName" className="mt-2 d-flex justify-content-end custom-margin">
               <button name="lastname" type="submit" className="btn btn-success" onClick={e => logIn(e)}>Submit</button>
             </div>
           </form>
