4fe21ff1b96a6db56cb260c7a1e0eee7ad539c99
Andy Park <apark1214@gmail.com>
2021-07-22 21:23:08 -0700

    making backend to retrieve all cardTitle

--- server/index.js
+++ server/index.js
@@ -112,17 +112,19 @@ app.post('/api/addCard', async (req, res, next) => {
 });
 
 // APP get to retrieve cardId
-app.get('/api/cardIdRetrieve',async(req,res,next)=>{
-  try{
-    const sql= `
+app.get('/api/cardIdRetrieve', async (req, res, next) => {
+  try {
+    const sql = `
       select *
       from "activities"
-    `
+    `;
+    const result = await db.query(sql);
+    res.status(201).json(result);
+  } catch (err) {
+    console.error(err);
   }
-  const result = await db.query(sql);
-  console.log(result)
 
-})
+});
 
 // APP GET to retrieve Data
 app.get('/api/retrieve', async (req, res, next) => {
