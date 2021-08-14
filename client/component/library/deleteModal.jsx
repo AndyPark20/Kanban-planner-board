1b383be33174e5d8f97d282e644d7e0f7c24bb22
Andy Park <apark1214@gmail.com>
2021-08-13 22:43:32 -0700

    making modal for confirmation

--- client/component/library/deleteModal.jsx
+++ client/component/library/deleteModal.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const DeleteModal = () => {
 
   return (
-    <div><h1>Hello</h1></div>
+    <div className="deleteModal">
+      <h3 className="deleteTitle">Are you Sure you want to delete this card?</h3>
+      <div className="button-layout">
+      <button type="click" className ="btn btn-danger">Yes</button>
+      <button type="click" className="btn btn-warning">No</button>
+      </div>
+    </div>
   );
 };
 
