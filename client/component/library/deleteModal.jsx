3469855a4e20162714569acd7d075a2ef07edab2
Andy Park <apark1214@gmail.com>
2021-08-13 22:52:20 -0700

    creating front end for modal confirmation

--- client/component/library/deleteModal.jsx
+++ client/component/library/deleteModal.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const DeleteModal = () => {
+
+  const [closeConfirmation, updateConfirmation] = useState(null);
+
+  // function for ClassName to hide and unhide the confirmation modal
+  const hideConfirmationModal = () => {
+    updateConfirmation(true);
+  };
+
+  // function to update closeConfirmation state
+  const updateConfirmationState = () => {
+    updateConfirmation(true);
+  };
 
   return (
     <div className="deleteModal">
       <h3 className="deleteTitle">Are you Sure you want to delete this card?</h3>
       <div className="button-layout">
-      <button type="click" className ="btn btn-danger">Yes</button>
-      <button type="click" className="btn btn-warning">No</button>
+        <button type="click" className="btn btn-danger danger">Yes</button>
+        <button type="click" className="btn btn-warning warning" onClick={updateConfirmationState}>No</button>
       </div>
     </div>
   );
