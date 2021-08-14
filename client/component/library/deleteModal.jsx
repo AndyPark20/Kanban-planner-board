1c6f4cb5646346f9cc7e050c7f6dcff61e6319fe
Andy Park <apark1214@gmail.com>
2021-08-14 00:08:42 -0700

    sending card numbers to the backend

--- client/component/library/deleteModal.jsx
+++ client/component/library/deleteModal.jsx
@@ -2,6 +2,16 @@ import React, { useEffect, useState } from 'react';
 
 const DeleteModal = ({ updateConfirmationModal, confirmationModal, columnNumber, cardNumber }) => {
 
+  // delete card by calling backend
+  const deleteCard = async () => {
+    const deleteCard = await fetch(`api/delete/${columnNumber}/${cardNumber}`, {
+      method: 'Delete',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+  };
+
   // function for ClassName to hide and unhide the confirmation modal
   const hideConfirmationModal = () => {
     if (confirmationModal) {
@@ -15,7 +25,7 @@ const DeleteModal = ({ updateConfirmationModal, confirmationModal, columnNumber,
     <div className={hideConfirmationModal()}>
       <h3 className="deleteTitle">Are you Sure you want to delete this card?</h3>
       <div className="button-layout">
-        <button type="click" className="btn btn-danger danger">Yes</button>
+        <button type="click" className="btn btn-danger danger" onClick={deleteCard}>Yes</button>
         <button type="click" className="btn btn-warning warning" onClick={() => updateConfirmationModal(true)}>No</button>
       </div>
     </div>
