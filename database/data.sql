07207d9d81320e7bf3c865a4fbd4033175b8e1c8
AndyPark20 <apark1214@gmail.com>
2021-08-16 01:44:30 +0000

    re working nested loops into hashmaps

--- database/data.sql
+++ database/data.sql
@@ -1,2 +1,2 @@
-insert into "record" ("activityId", "cardId","columnId","record","description", "time")
-values ('1','2','3','TEST','TESTING','123');
+insert into "users" ("firstName", "lastName","userName","password")
+values ('Andy','Park','123','$argon2i$v=19$m=4096,t=3,p=1$NFu4P3uZhL1zOEZ1Eg2qtw$TftOldPCkdlq2LE6Xv/3zLQwMPEcDb3qXXAzOKi3lyU');
